a153a2aa0a33c65b71cc3d637d3f60dae029f745
Almaz <iheardthatwas@gmail.com>
2022-10-17 05:37:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Categories from "./components/Categories";
+import Header from "./components/Header";
+import NFTBlock from "./components/NFTBlock";
+import Sort from "./components/Sort";
+
+import "./scss/app.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все NFT</h2>
+          <div class="content__items">
+            <NFTBlock title="Mac&chease" price="300" />
+            <NFTBlock title="Paper" price={200} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
